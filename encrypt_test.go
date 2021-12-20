1a9859e8f3ec69c01ddebf04400a5147b66709b6
Derek Smith <drsmith.phys@gmail.com>
2021-12-19 23:33:39 -0600

    feat(1.2): initial support for vault encryption 1.2 with vault-id
    Signed-off-by: Derek Smith <drsmith.phys@gmail.com>

--- encrypt_test.go
+++ encrypt_test.go
@@ -5,12 +5,12 @@ import (
 	"testing"
 )
 
-func Test_Encrypt(t *testing.T) {
+func Test_Encrypt_V11(t *testing.T) {
 	password := "asdf"
 	body := "secret"
 	var encrypted string
 	var err error
-	encrypted, err = Encrypt(body, password)
+	encrypted, err = Encrypt(body, password, "")
 	assert.NoError(t, err)
 
 	var result string
@@ -32,4 +32,59 @@ func Test_Encrypt_v2(t *testing.T) {
 	result, err = Decrypt(encrypted, password)
 	assert.NoError(t, err)
 	assert.Equal(t, body, result)
+}
+
+func Test_Encrypt_V12(t *testing.T) {
+	password := "asdf"
+	body := "secret"
+	var encrypted string
+	var err error
+	encrypted, err = Encrypt(body, password, "test")
+	assert.NoError(t, err)
+
+	var result string
+	result, err = Decrypt(encrypted, password)
+	assert.NoError(t, err)
+	assert.Equal(t, body, result)
+}
+
+func Test_encryptV11(t *testing.T) {
+	password := "asdf"
+	body := "secret"
+	var encrypted string
+	var err error
+	encrypted, err = encryptV11(body, password)
+	assert.NoError(t, err)
+
+	var result string
+	result, err = Decrypt(encrypted, password)
+	assert.NoError(t, err)
+	assert.Equal(t, body, result)
+}
+
+func Test_encryptV12(t *testing.T) {
+	password := "asdf"
+	body := "secret"
+	vaultID := "label"
+	var encrypted string
+	var err error
+	encrypted, err = encryptV12(body, password, vaultID)
+	assert.NoError(t, err)
+
+	var result string
+	result, err = Decrypt(encrypted, password)
+	assert.NoError(t, err)
+	assert.Equal(t, body, result)
+}
+
+func Test_checkvaultID(t *testing.T) {
+	var err error
+	err = checkVaultID("")
+	assert.NoError(t, err)
+
+	err = checkVaultID("1-)90$#98klascalkkDADQXASdasd=-=+_+_=-=")
+	assert.NoError(t, err)
+
+	err = checkVaultID("a;b")
+	assert.Error(t, err)
 }
